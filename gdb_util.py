5fba1fae0d893f4fee04a8ed72d3172b2ec991e2
hebust-fengyu <hebust-fengyu@outlook.com>
2023-08-22 16:46:12 +0800

    修改command结构，增加添加新命令便利性

--- gdb_util.py
+++ gdb_util.py
@@ -3,7 +3,7 @@
 
 import gdb
 import sys
-
+import inspect
 
 class MyCommand(gdb.Command):
     def __init__(self):
@@ -18,17 +18,19 @@ class MyCommand(gdb.Command):
         cv_arg = gdb.string_to_argv(arg)
         print(cv_arg)
 
-class PointerCommand(gdb.Command):
+class BaseCommand(gdb.Command):
+    def __init__(self, cmd_str, cmd_type = gdb.COMMAND_USER):
+        super(BaseCommand, self).__init__(cmd_str, cmd_type)
+
+class PointerCommand(BaseCommand):
     '''print pointer arg
-Usage: print_pointer arg [arg_type]
+Usage: (print_pointer | pp) arg [arg_type]
 Output:
-      arg : arg type value
+    arg : arg type value
     '''
-    def __init__(self):
-        if int(sys.version[0]) >= 3:
-            super().__init__('print_pointer', gdb.COMMAND_USER)
-        else:
-            super(PointerCommand, self).__init__('print_pointer', gdb.COMMAND_USER)
+    def __init__(self, cmd_str = "print_pointer", cmd_type = gdb.COMMAND_USER):
+        super(PointerCommand, self).__init__(cmd_str, cmd_type)
+    
     def invoke(self, argv, from_tty):
         args = gdb.string_to_argv(argv)
         pointer_v = args[0]
@@ -36,27 +38,33 @@ Output:
         pointer_type = gdb.lookup_type(args[1]).pointer() if len(args) >= 2 else pointer.type
         print("{} : {}".format(pointer, pointer.cast(pointer_type).dereference()))
 
-class PrintTypeCommand(gdb.Command):
-    '''    print elements types
-Usage: print_type arg1 arg2 arg3 ...
-Output:
-     [arg1] type is [type]
-     [arg2] type is [type]
-     [arg3] type is [type]
-    '''
+class AliasPointerCommand(PointerCommand):
     def __init__(self):
-        if int(sys.version[0]) >= 3:
-            super().__init__("print_type", gdb.COMMAND_USER)
-        else:
-            super(PrintTypeCommand, self).__init__("print_type", gdb.COMMAND_USER)
+        super(AliasPointerCommand, self).__init__("pp")
+
+class PrintTypeCommand(BaseCommand):
+    '''print elements types
+Usage: (print_type | pt) arg1 arg2 arg3 ...
+Output:
+    [arg1] type is [type]
+    [arg2] type is [type]
+    [arg3] type is [type]
+    '''
+    def __init__(self, cmd_str = "print_type", cmd_type = gdb.COMMAND_USER):
+        super(PrintTypeCommand, self,).__init__(cmd_str, cmd_type)
 
     def invoke(self, argv, from_tty):
         vals = gdb.string_to_argv(argv)
         for val in vals:
             gdb_val = gdb.parse_and_eval(val)
             print("[{}] type is [{}]".format(val, gdb_val.type))
-PrintTypeCommand()
-MyCommand()
-PointerCommand()
 
+class AliasPrintTypeCommand(PrintTypeCommand):
+    def __init__(self):
+        super(AliasPrintTypeCommand, self).__init__("pt")
+
+module_mem = inspect.getmembers(sys.modules[__name__])
+for name, obj in module_mem:
+    if inspect.isclass(obj) and issubclass(obj, BaseCommand) and obj is not BaseCommand:
+        obj()
 
